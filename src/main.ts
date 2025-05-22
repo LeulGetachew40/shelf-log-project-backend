706455b4db233d38e504ec116df73c6de38d4134
LeulGetachew40 <117739996+LeulGetachew40@users.noreply.github.com>
2025-05-22 07:07:17 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Handler, Context, Callback } from 'aws-lambda';
-import { configure as serverlessExpress } from '@vendia/serverless-express';
-
-let server: Handler;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.init();
-
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  await app.listen(3000);
 }
-
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  server = server ?? (await bootstrap());
-  return server(event, context, callback);
-};
+bootstrap();
