85b60f6d07d234a6fd1531a8f62feaab58f747c8
Leul Getachew <leulgetachew40@gmail.com>
2025-05-22 20:35:56 +0300

    enable cors for all origins

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-    app.enableCors({
-    origin: 'http://localhost:5173',
+  app.enableCors({
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type',
   });
-  
+
   await app.listen(3000);
 }
 bootstrap();
